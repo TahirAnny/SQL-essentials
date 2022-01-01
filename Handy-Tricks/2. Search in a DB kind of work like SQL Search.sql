b1263069c493cd1e2845f94defada9c20e394c9a
Tahira Bishwas Anny <35254801+TahirAnny@users.noreply.github.com>
2022-01-01 23:00:25 +0600

    Update 2. Search in a DB kind of work like SQL Search.sql

--- Handy-Tricks/2. Search in a DB kind of work like SQL Search.sql
+++ Handy-Tricks/2. Search in a DB kind of work like SQL Search.sql
@@ -1,7 +1,7 @@
+## 01
+
 DECLARE @Search varchar(255)='Student'
 DECLARE @ObjType varchar(255)=''
-
-
 
 SELECT DISTINCT o.name AS Object_Name,o.type_desc,m.definition 
 FROM sys.sql_modules        m 
@@ -9,3 +9,12 @@ INNER JOIN sys.objects  o ON m.object_id=o.object_id
 WHERE m.definition Like '%'+@Search+'%'
 	AND ( @ObjType='' OR o.type_desc=@ObjType)
 ORDER BY 2,1
+
+## 02
+SELECT  A.NAME
+	   ,A.TYPE
+	   ,B.TEXT
+FROM SYSOBJECTS  A (NOLOCK)
+JOIN SYSCOMMENTS B (NOLOCK)  ON A.ID = B.ID
+WHERE B.TEXT LIKE '%YOUR REQUIRED TEXT%'  AND A.TYPE = 'P'                    
+ORDER BY A.NAME
