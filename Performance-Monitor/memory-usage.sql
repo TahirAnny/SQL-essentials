5bf99f0e9850db2c5d64e58cede63110d202b144
Tahira Bishwas Anny <35254801+TahirAnny@users.noreply.github.com>
2022-04-04 19:24:07 +0600

    Create memory-usage.sql

---
+++ Performance-Monitor/memory-usage.sql
@@ -0,0 +1,12 @@
+/*
+  Phy_Memory_usedby_Sqlserver_MB= physical_memory_in_use
+  Total_Memory_UsedBySQLServer_MB= virtual_address_space_committed
+*/
+
+SELECT
+(physical_memory_in_use_kb/1024)Phy_Memory_usedby_Sqlserver_MB,
+(locked_page_allocations_kb/1024 )Locked_pages_used_Sqlserver_MB,
+(virtual_address_space_committed_kb/1024 )Total_Memory_UsedBySQLServer_MB,
+process_physical_memory_low,
+process_virtual_memory_low
+FROM sys. dm_os_process_memory
